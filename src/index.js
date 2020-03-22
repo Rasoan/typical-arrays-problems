60ea347467a06120f51547ec01cf1c10937407e1
Araik Rasoyan <araikrasoian@gmail.com>
2020-03-22 16:23:18 +0300

    init: start tipical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,69 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  let variable;
+
+  if ( Array.isArray(array) && array.length > 0 ) {
+   variable = array[0];
+  }
+  else return 0;
+
+  array.forEach( function f(element, index, arr) {
+       variable = (variable > element ? element: variable); 
+  });
+  return variable;
 }
+
+
 
 exports.max = function max (array) {
-  return 0;
+  let variable;
+
+  if ( Array.isArray(array) && array.length > 0 ) {
+      variable = array[0];
+   }
+  else return 0;
+
+  array.forEach( function f(element, index, arr) {
+      variable = (variable < element ? element: variable);  
+  });
+  return variable;
 }
 
+
+
+
+
+
 exports.avg = function avg (array) {
-  return 0;
+    let max;
+    let variable;
+    let sum = 0;
+
+    if ( !( Array.isArray(array) && array.length > 0 ) ) 
+      return 0;
+    
+  
+
+    
+    array.forEach( function f(element, index, arr) {
+        sum += element;  
+    }); 
+    
+
+     return  sum / array.length;
 }
+
+
+
+
+
+
+  
+  
+  
+  
+//console.log(exports.min([27,27,-4,-10,17,34,-26,-4,19,22,34,14,5,20,-23,-1,-35,25,2,-35]));
+  
+
+  
+  
